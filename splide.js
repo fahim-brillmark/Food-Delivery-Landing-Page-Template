078abbea2fc5d8c6630c81fb37490c35db90c8bb
Fahim2000 <fahim.193902022@green.ac.bd>
2024-09-27 02:33:29 +0600

    Search by food section done

--- splide.js
+++ splide.js
@@ -1,17 +1,31 @@
 document.addEventListener("DOMContentLoaded", function () {
-    new Splide(".splide", {
-      type: "loop",
-      perPage: 5,
-      perMove: 1,
-      pagination: false,
-      breakpoints: {
-        1350: {
-          perPage: 2,
-        },
-        800: {
-          perPage: 1,
-        },
+  new Splide(".splide", {
+    type: "loop",
+    perPage: 5,
+    perMove: 1,
+    pagination: false,
+    breakpoints: {
+      1350: {
+        perPage: 2,
       },
-    }).mount();
+      800: {
+        perPage: 1,
+      },
+    },
+  }).mount();
 
-  });
+  new Splide(".search-by-food", {
+    type: "loop",
+    perPage: 6,
+    perMove: 1,
+    pagination: false,
+    breakpoints: {
+      1350: {
+        perPage: 2,
+      },
+      800: {
+        perPage: 1,
+      },
+    },
+  }).mount();
+});
